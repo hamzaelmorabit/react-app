b3821bd724e27ced53c128015fa24ee49e8b14d2
hamza EL MORABIT <hamzamorabit123@gmail.com>
2021-03-31 16:10:57 +0100

    get data from server

--- App.js
+++ App.js
@@ -3,7 +3,21 @@ import React, { Component } from "react";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Main from "./components/MainComponent";
+import axios from "axios";
+
 export class App extends Component {
+  // Define what props.theme will look like
+  state = {
+    users: [],
+  };
+
+  async componentDidMount() {
+    const { data: users } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    this.setState({ users });
+    // console.log(data);
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +27,46 @@ export class App extends Component {
           </div>
         </Navbar>
         {/* <MenuComponent /> */}
-        <Main />
+        {/* <Main /> */}
+        <div style={{ marginTop: "4%" }} className="panel panel-primary">
+          <style></style>
+          <div>
+            <h2 style={{ textAlign: "center" }} className="h4 mb-4">
+              Liste des personnes
+            </h2>
+          </div>
+          <div style={{ marginTop: "3%" }} className="panel-body">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>title</th>
+                  <th>Delete</th>
+                  <th>Détails</th>
+                </tr>
+              </thead>
+              {this.state.users.map((user) => (
+                <>
+                  <tbody>
+                    <tr>
+                      <td>{user.title}</td>
+
+                      <td>
+                        <button className="btn btn-danger">Delete</button>&nbsp;
+                        {/* <button className="btn btn-info">Modifier</button> */}
+                      </td>
+
+                      <td>
+                        <button className="btn btn-info btn-block">
+                          Détails
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </>
+              ))}
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
