751aab700e8eb11b18f2bd7ce8c409cb8946eb10
hamza EL MORABIT <hamzamorabit123@gmail.com>
2021-04-02 11:39:28 +0100

    React Router

--- App.js
+++ App.js
@@ -5,24 +5,22 @@ import { Navbar, NavbarBrand } from "reactstrap";
 import Main from "./components/MainComponent";
 import Service from "./service/Service";
 import axios from "axios";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
+import { BrowserRouter } from "react-router-dom";
 export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        {/* <MenuComponent /> */}
-        <Main />
-        {/* <Service /> */}
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          {/* <MenuComponent /> */}
+          <Main />
+          {/* <Service /> */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { DishdetailComponent } from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
 import { Menu } from "./MenuComponent";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import { Switch, Route, Redirect } from "react-router-dom";
+
 class Main extends Component {
   constructor(props) {
     console.log(`Main component Constructor Invoked`);
@@ -22,16 +27,31 @@ class Main extends Component {
   onDishSelect(dish) {
     this.setState({ selectedDishId: dish });
   }
+
+  HomePage = () => {
+    return <Home />;
+  };
+
   render() {
     console.log(`Main component render Invoked`);
 
     return (
       <div className="container">
-        <Menu
+        <Header />
+        <Switch>
+          <Route path="/home" component={this.HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
+        {/* <Menu
           dishes={this.state.dishes}
           onClick={(selectedDish) => this.onDishSelect(selectedDish)}
-        />
-        <div className="row">
+        /> */}
+        {/* <div className="row">
           <div className="col-12 col-md-5 m-1">
             <DishdetailComponent
               selectedDish={this.state.dishes.find(
@@ -39,7 +59,8 @@ class Main extends Component {
               )}
             />
           </div>
-        </div>
+        </div> */}
+        <Footer />
       </div>
     );
   }
